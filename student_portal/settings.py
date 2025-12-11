71dae84cfaf9e4296c25fbd04f91527d307241c4
SA <sa@SAs-MacBook-Pro.local>
2025-12-12 02:41:42 +0300

    This new

--- student_portal/settings.py
+++ student_portal/settings.py
@@ -153,7 +153,3 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-import os
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
